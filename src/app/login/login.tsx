7e1b3df0b6aa29652300c9dc424d18b9b8f5baf0
HeshanPravintha <31812933+HeshanPravintha@users.noreply.github.com>
2023-04-21 11:11:55 +0530

    login

---
+++ src/app/login/login.tsx
@@ -0,0 +1,12 @@
+export default function Login() {
+    
+    return (
+      <div>
+        <h1>Log in here</h1>
+        <form>
+          <input name="email" placeholder="Your email address" />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }

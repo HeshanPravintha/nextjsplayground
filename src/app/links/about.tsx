416203228d9a9c8e8096808dbb08fd2787831b97
HeshanPravintha <31812933+HeshanPravintha@users.noreply.github.com>
2023-04-21 11:07:40 +0530

    Link about page

---
+++ src/app/links/about.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      
+      <div>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+        <Link href="/blog/123">
+          <a>My Blog Post</a>
+        </Link>
+      </div>
+    </div>
+  );
+}
